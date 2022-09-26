48f8528dc30b21d48da398ffde614c0a4f8ff292
Даниил Рудь <ruddanil99@gmail.com>
2022-09-26 04:13:01 +0400

    Initial commit

---
+++ CalcApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using CalcApp;
+
+Console.WriteLine("Enter the first number:");
+var a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the operation sign:");
+var op = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+var b = Convert.ToDouble(Console.ReadLine());
+
+var calculator = new Calculator();
+var result = calculator.Calc(a, b, op);
+
+Console.WriteLine("Result: " + result);
