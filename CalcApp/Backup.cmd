8afd3f4ca97e4bd2cdd108857a324d8704d9d1a4
Даниил Рудь <ruddanil99@gmail.com>
2022-10-19 20:12:29 +0400

    Update Backup.cmd

--- CalcApp/Backup.cmd
+++ CalcApp/Backup.cmd
@@ -1,1 +1,1 @@
-C:\"Program Files"\7-Zip\7z.exe a -mx9 C:\JenkinsBackup\backup C:\ProgramData\Jenkins\.jenkins\workspace\CalcApp\CalcApp\bin\Debug\net6.0
+C:\"Program Files"\7-Zip\7z.exe a -mx9 C:\JenkinsBackup\backup"%1".7z C:\ProgramData\Jenkins\.jenkins\workspace\CalcApp\CalcApp\bin\Debug\net6.0
